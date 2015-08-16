c41e860a61ca9ae30aad6632c9d1f3428238858f
Neon <neon1226@yandex.ru>
2015-08-16 19:23:12 +0400

    Added GasStation Plugin

---
+++ plugins/gasstation/sh_plugin.lua
@@ -0,0 +1,7 @@
+PLUGIN.name = "Gas Station"
+PLUGIN.author = "_FR_Starfox64 (NS 1.0), Neon (NS 1.1)"
+PLUGIN.desc = "A new way to put gas in your vehicle!"
+
+PLUGIN.price = 2
+
+nut.util.include("sv_plugin.lua")
